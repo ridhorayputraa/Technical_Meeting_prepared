2208e8edaa35c0e024421f9a28145de355fe25bf
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-06-01 19:09:45 +0700

    Adding Laravel Jetsream to Projects

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -20,3 +20,13 @@ Route::get('/', function () {
 Route::get('/about', function(){
     return view('about/about');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
