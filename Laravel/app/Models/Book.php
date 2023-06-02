43e1aa3e170ea5b98a1e665ce9ec06a6c3b1a055
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-06-02 13:37:12 +0700

    Migrate: After migrating and creating field for books

--- Laravel/app/Models/Book.php
+++ Laravel/app/Models/Book.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
 
 class Book extends Model
 {
@@ -13,5 +15,11 @@ class Book extends Model
     protected $guarded = ['id'];
 
     // Epoch UNIX timestamps
-    // public function getCreated
+    public function getCreatedAttribute($value) {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function GetPicture(){
+        return url('') . Storage::url($this->attributes['Thumbnail']);
+    }
 }
--- Laravel/database/migrations/2023_06_02_060153_create_books_table.php
+++ Laravel/database/migrations/2023_06_02_060153_create_books_table.php
@@ -16,6 +16,22 @@ class CreateBooksTable extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+
+            // relatons
+            $table->foreignId('genre_id');
+            $table->foreignId('user_id');
+            $table->foreignId('author_id');
+
+            // Field
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('slug')->unique();
+            $table->integer('price')->nullable();
+            $table->double('rating')->nullable();
+            $table->string('language')->nullable();
+            $table->text('thumbnail')->nullable();
+
+            $table->softDeletes();
         });
     }
 
